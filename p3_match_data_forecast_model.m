f3edf330bfcd5157d3967cdfb6bcb526782168b8
ARSgesong <139445082+ARSgesong@users.noreply.github.com>
2025-02-28 21:01:26 +0800

    Update p3_match_data_forecast_model.m
    We have changed all Chinese annotations into English

--- p3_match_data_forecast_model.m
+++ p3_match_data_forecast_model.m
@@ -1,21 +1,17 @@
 clear all
 %%
-GEMS_NO2_path = '/data01/sg/数据处理备份/GEMS-NO2-L3/p2/';
-GEMS_HCHO_path = '/data01/sg/数据处理备份/GEMS-HCHO-L3/p2/';
-GEMS_UV_path = '/data01/sg/数据处理备份/GEMS-UV-L3/p2/';
-AUX_path = '/data01/sg/2023-静止卫星臭氧光化学反演/中间数据/AuxiliaryData/';
+GEMS_NO2_path = '/.../'; %The filepath that stored the processed L3 NO2 datasets
+GEMS_HCHO_path = '/.../'; %The filepath that stored the processed L3 HCHO datasets
+GEMS_UV_path = '/.../'; %The filepath that stored the processed L3 UV datasets
+AUX_path = '/.../'; %The filepath that stored the processed L3 auxiliary datasets
 
-O3_GRD_path = '/data01/sg/2023-静止卫星臭氧光化学反演/中间数据/GRDdata/';
-
-pSaveDatapath = '/data01/sg/2023-静止卫星臭氧光化学反演/匹配数据集_卫星全_Forecast/';
-% pSaveDatapath = '/data01/sg/2023-静止卫星臭氧光化学反演/匹配数据集_MDA8_ratio/';
-
-pSaveSDatapath = '/data01/sg/2023-静止卫星臭氧光化学反演/训练数据集_Forecast/';
-% pSaveSDatapath = '/data01/sg/2023-静止卫星臭氧光化学反演/训练数据集_ratio/';
+O3_GRD_path = '/.../'; %The filepath that stored the processed L3 monitored air pollution datasets
+pSaveDatapath = '/.../'; %The filepath to store the matched dataset
+pSaveSDatapath = '/.../'; %The filepath to store the matched dataset with site observations
 
 Filelist = dir([GEMS_NO2_path,'*.mat']);
 pf = length(Filelist);
-%分辨率网格数量
+%specify the spatial resolution
 fb1 = 0.05;
 fb2 = 0.05;
 CONUSC = round(roundn((135-73)/fb1,-2));
