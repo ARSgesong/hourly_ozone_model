923a7c6fc38f45db85e131b26244a49935b87d43
ARSgesong <139445082+ARSgesong@users.noreply.github.com>
2025-02-28 20:45:54 +0800

    Update p1_s2_satellite_data_UV.m
    We have changed all Chinese annotations into English

--- p1_s2_satellite_data_UV.m
+++ p1_s2_satellite_data_UV.m
@@ -1,18 +1,19 @@
 clear all
-%% 读取和处理网格化卫星数据数据
-pLoadDatapath = '/data01/sg/数据处理备份/GEMS-UV-L3/p1/';
-pSaveDatapath = '/data01/sg/数据处理备份/GEMS-UV-L3/p2/';
+%%  read and process the gridded satellite dataset
+pLoadDatapath = '/.../'; The filepath that stored the processed L2 UV datasets
+pSaveDatapath = '/.../'; The filepath to store the processed L3 UV datasets
+
 mFilelistUV = dir([pLoadDatapath,'*.mat']);
 pf = length(mFilelistUV);
 j = -1;
 
-%% 指定中国的范围
+%% designate the spatial range
 GRID_LAT_RANGE = [18,54];
 GRID_LONG_RANGE = [73,135];
 GRID_GAP_LAT = 0.05;
 GRID_GAP_LONG = 0.05;
 
-%% 格网
+%% gridding
 GRID_LAT_RANGE = minmax(GRID_LAT_RANGE);
 GRID_LONG_RANGE = minmax(GRID_LONG_RANGE);
 GRID_BIN_LAT = GRID_LAT_RANGE(1):GRID_GAP_LAT:GRID_LAT_RANGE(2);
@@ -23,13 +24,13 @@ grid_row = length(GRID_BIN_LAT);
 
 total_num = pf;
 cnt = 1;
-for i = 2981:pf
-    % 避免重复归类计算
+for i = 1:pf
+    % avoid repetations
     if i <= j-1
         continue
     end
     
-%     disp("执行文件数");
+%     disp("number of processed documents");
 %     disp(i);
 %     disp(mFilelistNO2(i))
     pYearStr = mFilelistUV(i).name(1:4);
